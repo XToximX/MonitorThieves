0f41038f7d9ef7dcfee4ac65de8ee0b6b5b84aec
XToximX <tomek.lech.22@gmail.com>
2025-02-21 18:56:58 +0100

    added menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
@@ -16,7 +17,7 @@ public class MainMenu : MonoBehaviour
 
     public void PlayBtn()
     {
-        Debug.Log("Play");
+        SceneManager.LoadScene(1);
     }
     
     public void SettingsBtn()
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [Header("Buttons")]
+    [SerializeField] GameObject resumeBtn;
+    [SerializeField] GameObject mainMenuBtn;
+    [SerializeField] GameObject quitBnt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        resumeBtn.SetActive(false);
+        mainMenuBtn.SetActive(false);
+        quitBnt.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!resumeBtn.activeSelf)
+            {
+                Time.timeScale = 0f;
+                resumeBtn.SetActive(true);
+                mainMenuBtn.SetActive(true);
+                quitBnt.SetActive(true);
+            }
+            else
+                Resume();
+            
+        }
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+        resumeBtn.SetActive(false);
+        mainMenuBtn.SetActive(false);
+        quitBnt.SetActive(false);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
